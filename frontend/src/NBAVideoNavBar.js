f7080b9069924256da0d3514401e989b0b98bf31
Avyay Varadarajan <avyayv@gmail.com>
2021-09-05 22:33:30 -0700

    removed default navbar image

--- frontend/src/NBAVideoNavBar.js
+++ frontend/src/NBAVideoNavBar.js
@@ -7,13 +7,6 @@ class NBAVideoNavBar extends React.Component {
             <Navbar bg="dark" variant="dark">
                 <Container>
                 <Navbar.Brand href="#home">
-                    <img
-                    alt=""
-                    src="/logo.svg"
-                    width="30"
-                    height="30"
-                    className="d-inline-block align-top"
-                    />{' '}
                 NBA Video Searcher 
                 </Navbar.Brand>
                 </Container>
